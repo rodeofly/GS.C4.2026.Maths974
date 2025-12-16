59022e5a74558604a0763c754733ccfd7a242461
rodeofly <florian.tobe@gmail.com>
2025-12-16 14:17:01 +0400

    v.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   srcDir: 'src',
   root: '.',
+  // URL de ton site GitHub Pages
+  site: 'https://rodeofly.github.io',
+  
+  // Nom de ton dépôt (attention à la casse, c'est important !)
+  base: '/GS.C4.2026.Maths974',
+
   server: {
     host: true,
   },
